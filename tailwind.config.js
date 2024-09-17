6dfa4a6652c9558e60c19eb4fe637127d266d6ad
marynag <mgrabchak1109@gmail.com>
2024-09-17 09:16:12 +0300

    set styles config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
     './node_modules/flowbite/**/*.js',
   ],
   theme: {
-    extend: {},
+    colors: {
+      darkGreen: '#2A5831',
+      lightGreen: '#76B947',
+      lightTextGrey: '#F2F2F2',
+      darkTextGrey: '#333333',
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif'],
+      play: ['Play', 'sans-serif'],
+    },
+    backgroundImage: {
+      'custom-gradient':
+        'linear-gradient(180deg, #2F5233 0%, #3B6041 36.65%, #568162 84.5%, #6C9B7C 100%)',
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
