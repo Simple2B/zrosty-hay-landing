99e088c22c6998b57f5e5c7d963af6956655e3cf
marynag <mgrabchak1109@gmail.com>
2024-09-18 09:40:20 +0300

    set default light theme

--- src/base.ts
+++ src/base.ts
@@ -6,27 +6,27 @@ export interface HTMXEventDetail {
 }
 
 const themeToggleDarkIcons = document.querySelectorAll(
-  '#theme-toggle-dark-icon',
+  '#theme-toggle-light-icon',
 );
 const themeToggleLightIcons = document.querySelectorAll(
-  '#theme-toggle-light-icon',
+  '#theme-toggle-dark-icon',
 );
 
 // Change the icons inside the button based on previous settings
 if (
-  localStorage.getItem('color-theme') === 'dark' ||
+  localStorage.getItem('color-theme') === 'light' ||
   (!('color-theme' in localStorage) &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
+    window.matchMedia('(prefers-color-scheme: light)').matches)
 ) {
   themeToggleLightIcons.forEach(function (el) {
     el.classList.remove('hidden');
   });
-  document.documentElement.classList.add('dark');
+  document.documentElement.classList.add('light');
 } else {
   themeToggleDarkIcons.forEach(function (el) {
     el.classList.remove('hidden');
   });
-  document.documentElement.classList.remove('dark');
+  document.documentElement.classList.remove('light');
 }
 
 const themeToggleButtons = document.querySelectorAll('#theme-toggle');
@@ -44,22 +44,22 @@ themeToggleButtons.forEach(function (themeToggleBtn) {
 
     // if set via local storage previously
     if (localStorage.getItem('color-theme')) {
-      if (localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('color-theme', 'dark');
-      } else {
-        document.documentElement.classList.remove('dark');
+      if (localStorage.getItem('color-theme') === 'dark') {
+        document.documentElement.classList.add('light');
         localStorage.setItem('color-theme', 'light');
+      } else {
+        document.documentElement.classList.remove('light');
+        localStorage.setItem('color-theme', 'dark');
       }
 
       // if NOT set via local storage previously
     } else {
-      if (document.documentElement.classList.contains('dark')) {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
-      } else {
-        document.documentElement.classList.add('dark');
+      if (document.documentElement.classList.contains('light')) {
+        document.documentElement.classList.remove('light');
         localStorage.setItem('color-theme', 'dark');
+      } else {
+        document.documentElement.classList.add('light');
+        localStorage.setItem('color-theme', 'light');
       }
     }
   });
